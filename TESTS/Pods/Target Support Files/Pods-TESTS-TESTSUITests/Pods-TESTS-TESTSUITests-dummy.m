5c56f1c6f37c34513ef910ef0fa8258cdc3d71a3
MoringStr <MoringStr010@gmail.com>
2020-02-03 21:41:54 +0800

    snipkit

---
+++ TESTS/Pods/Target Support Files/Pods-TESTS-TESTSUITests/Pods-TESTS-TESTSUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TESTS_TESTSUITests : NSObject
+@end
+@implementation PodsDummy_Pods_TESTS_TESTSUITests
+@end
---
+++ TESTS/Pods/Target Support Files/Pods-TESTSTests/Pods-TESTSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TESTSTests : NSObject
+@end
+@implementation PodsDummy_Pods_TESTSTests
+@end
